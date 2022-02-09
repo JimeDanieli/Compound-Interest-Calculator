e9951ef89b9f78230b74df19b3dcf6cc03b94bbb
Jime <jimedanieli.09@gmail.com>
2022-02-09 11:58:18 -0300

    Calculator working

--- interest-calculator/src/App.js
+++ interest-calculator/src/App.js
@@ -3,22 +3,17 @@ import {Formik, Form} from 'formik';
 import Input from './Components/Input';
 import Button from './Components/Button'
 
-const Container= styled.div`
-display:flex;
-justify-content:center;
-height:100%;
-align-items:center
-`
-const Section=styled.section`
-  background-color:#eee;
-  border-top:solid 7px palevioletred ;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3 px rgb(0,0,0,0.3)
-`
+const compoundInterest=(deposit,contribution,years, rate)=>{
+  let total= deposit
+  for(let i =0; i < years; i ++){
+    total=(total + contribution) * (rate + 1)
+  }
+  return Math.round(total)
+}
+
 function App() {
-  const handleSubmit=()=>{
-    
+  const handleSubmit=({deposit, contribution, years, rate})=>{
+    const val=compoundInterest(Number(deposit),Number(contribution),Number(years),Number(rate))
   }
   return (
     <Container>
